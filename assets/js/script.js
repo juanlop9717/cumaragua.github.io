1894971ec9cfae2b42208864f111f14a7be62eab
juanlop9717 <juanlop9717@gmail.com>
2017-10-06 15:27:32 -0400

    Add social section

--- assets/js/script.js
+++ assets/js/script.js
@@ -22,20 +22,21 @@ $(document).ready(function(){
         }
     })
 
-     alert("js activo")
      // Instagram Request
      let apiKey = "5e983c4725ec4fbea6ebdbe7be6fdeb2";
 
      $.ajax({
-      url: 'https://api.instagram.com/v1/users/self/media/recent/?access_token=5e983c4725ec4fbea6ebdbe7be6fdeb2',
+      url: 'https://api.instagram.com/v1/users/2279717056/media/recent/?access_token=289336980.5e983c4.4f8829875a6a44c1a402ea8cdb2985ad&code=89880681977544c4b4c3426c5bb66df1',
 
       error: function() {
-        alert('error');
+        alert("error")
       },
 
       success: function(data) {
-       alert('yes');
-       console.log(data)
+        console.log(data);
+        for(var i = 0 ; i < 10; i++){
+         $(`#box${i+1}`).attr("src", data.data[i].images.standard_resolution.url);
+        }      
       },
       type: 'GET',
       dataType: "jsonp"
